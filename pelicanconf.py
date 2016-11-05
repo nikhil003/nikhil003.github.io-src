8586ec6ddafedb51543ce540bb8a42d10f3978a9
Nikhil Garg <nikhil@Nikhils-MBP.home>
2016-11-05 19:09:07 +0800

    First commit.

---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Nikhil Garg'
+SITENAME = u'http://nikhil003.github.io'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Singapore'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+# STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico']
+# EXTRA_PATH_METADATA = {
+#     'extra/robots.txt': {'path': 'robots.txt'},
+#     'extra/favicon.ico': {'path': 'favicon.ico'}
+# }
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
