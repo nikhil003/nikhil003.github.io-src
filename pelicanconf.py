ba5d36ea33a35725879ca864e61ead3d1f222e6d
Nikhil Garg <nikhilgarg.gju@gmail.com>
2016-11-05 20:20:03 +0800

    some more

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,11 @@ SOCIAL = (('You can add links in your config file', '#'),
 #     'extra/favicon.ico': {'path': 'favicon.ico'}
 # }
 
-DEFAULT_PAGINATION = True
+DEFAULT_PAGINATION = 2
 
+ARTICLE_URL = '{slug}/'
+ARTICLE_SAVE_AS = '{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
